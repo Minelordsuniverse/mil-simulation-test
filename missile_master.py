0182b7972e6a023c062cc219027ac07fa03c7d0a
DefinitelyNotML <67429188+Minelordsuniverse@users.noreply.github.com>
2025-08-30 04:54:53 +0800

    Update missile_master.py

--- missile_master.py
+++ missile_master.py
@@ -1,8 +1,13 @@
 import argparse
 import os
 
-# Import each module from the respective files
+# Imports each module from the respective files
 import trajectory_simulation 
 import radar_coverage
 import interception_simulation
 import comparative_analysis
+
+def ensure_folders():
+    os.makedirs("inputs", exist_ok=True)
+    os.makedirs("outputs", exist_ok=True)
+
