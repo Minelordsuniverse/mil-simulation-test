614eb4d3fafbd5b3bcd17af0d96bcff070df52d0
DefinitelyNotML <67429188+Minelordsuniverse@users.noreply.github.com>
2025-08-30 05:11:17 +0800

    Update comparative_analysis.py

--- comparative_analysis.py
+++ comparative_analysis.py
@@ -2,3 +2,11 @@ import os
 import pandas as pd
 import matplotlib.pyplot as plt
 
+# using predetermined value inputs
+def generate_comparison_data():
+    data = {
+        "system": ["Blue SAM (Patriot)", "Red SAM (S-400)", "Blue Fighter (AMRAAM)", "Red Fighter (R-37)"],
+        "range_km": [70, 250, 100, 300],
+        "speed_mach": [4.0, 5.0, 4.0, 6.0]
+    }
+    return pd.DataFrame(data)
